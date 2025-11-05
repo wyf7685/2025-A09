31592b9e7ac632eb8cf215dff2d1cf4ecd4822fc
wyf7685 <wyf7685@163.com>
2025-11-05 11:05:46 +0800

    chore(build): update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -28,13 +28,7 @@ export default defineConfig({
         main: fileURLToPath(new URL('./index.html', import.meta.url)),
       },
       output: {
-        assetFileNames: (assetInfo) => {
-          // 对于 favicon.ico，保持其在根目录中
-          if (assetInfo.names.includes('favicon.ico')) {
-            return 'favicon.ico';
-          }
-          return 'assets/[hash][extname]';
-        },
+        assetFileNames: 'assets/[hash][extname]',
         chunkFileNames: 'assets/[hash].js',
         entryFileNames: 'assets/[hash].js',
         manualChunks: {
