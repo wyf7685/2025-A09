f7749179314cdcca9f063cf118e0d4c5d584ca05
wyf7685 <wyf7685@163.com>
2025-11-16 16:15:16 +0800

    feat(workflow): update build workflow

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
   readonly VITE_ICP_NUMBER: string;
   readonly VITE_POLICE_NUMBER: string;
+  readonly VITE_POLICE_STRING: string;
   readonly VITE_COMPANY_NAME: string;
 }
 
